fb10ce2370a541d99d5b2d180a085d9768bf4758
storymode7 <17mayanksinghal@gmail.com>
2018-04-16 02:23:16 +0530

    Fix flake8 errors in authorize.py

--- src/authorize.py
+++ src/authorize.py
@@ -3,8 +3,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import os
 
 if 'client_id' not in os.environ.keys():
-   os.environ['client_id'] = "No Key"
-   os.environ['client_secret'] = "No Key"
+    os.environ['client_id'] = "No Key"
+    os.environ['client_secret'] = "No Key"
     print('client_id and client_secret are not set, please add '
           'them in your .env file and do pipenv shell again.')
 
